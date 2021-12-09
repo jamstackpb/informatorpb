286e84f3c080072f9a77679130783901aa734bbb
Marcin Falkowski <falkowskimarcin98@gmail.com>
2021-12-09 15:23:49 +0100

    :christmas_tree:  :santa: Flipbook on NextJS
    Postawiony ten flipbook: https://github.com/Nodlik/StPageFlip
    
    Troszkę się nim pobawiliśmy.
    Markdowny zostały załadowane, dodane przyciski nawigacyjne, formatowanie markdowna na formatowania ze strony koła.
    Na pierwszy rzut oka wygląda okej. trzeba byłoby troszkę się z nim pobawić wyglądowo oraz popracować nad wyświetlanym tekstem.
    
    P.S wrzucam w celu sprawdzenia, czy to ma jakikolwiek sens. Nad flipbookiem opartym na jquery siedzieliśmy 4 dni i nie zdołaliśmy go nawet wyświetlić na naszej paczce startowej.  Odpalając przykład z gita tamta paczka pokazywała informacje o wersji demo.
    
    Co-Authored-By: AleksanderBondar <86482559+AleksanderBondar@users.noreply.github.com>

---
+++ apps/flipbook/README.md
@@ -0,0 +1,27 @@
+# Informator Politechniki Białostockiej
+
+Repozytorium nowego informatora Politechniki Białostockiej.
+
+## Jak zacząć
+
+Ściągnij te repozytorium
+```sh
+$ git clone https://github.com/jamstackpb/informatorpb
+```
+
+Zainstaluj zależoności
+```sh
+$ npm i
+```
+
+## Uruchamianie
+
+```sh
+$ npm run dev
+```
+
+## Technologie
+- markdown
+- TypeScript
+- ReactJS
+- NextJS
---
+++ apps/flipbook/ssg/flipbookcontent.ts
@@ -0,0 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import { marked } from 'marked';
+import DOMPurify from 'isomorphic-dompurify';
+
+export const getFlipBook = () => {
+    const pathToFolderProjects = path.join(process.cwd(), '..', '..', 'content', '2021', 'pages');
+    const projectsDirectoryFiles = fs.readdirSync(pathToFolderProjects);
+
+    const withGrayMatter = projectsDirectoryFiles.map((adf) => {
+        const realPath = path.join(pathToFolderProjects, adf);
+        const fileContents = fs.readFileSync(realPath).toString('utf-8');
+        const { data: changedToMatter, content } = matter(fileContents);
+        const dirty = marked(content);
+        const clean = DOMPurify.sanitize(dirty);
+        return {
+            changedToMatter,
+            clean,
+        };
+    });
+    return withGrayMatter;
+};
