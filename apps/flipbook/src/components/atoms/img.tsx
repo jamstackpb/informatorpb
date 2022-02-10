9080fb13857071a60a31f1834c8ad54a63108e5a
aexol <aexol@aexol.com>
2022-02-10 16:44:41 +0100

    :bug: removed image

--- apps/flipbook/src/components/atoms/img.tsx
+++ apps/flipbook/src/components/atoms/img.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image';
-
 export const Img = ({ src }: { src: string }) => {
-    return <Image unoptimized layout="responsive" width={'50%'} height={'50%'} loader={(e) => e.src} src={src} />;
+    return <img src={src} />;
 };
