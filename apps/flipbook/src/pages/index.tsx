798aa6838f849da7e3543d8b34652aea015ea90e
SMATEO49 <smateusz49@interia.eu>
2022-02-21 15:54:00 +0100

    Cleaned unused

--- apps/flipbook/src/pages/index.tsx
+++ apps/flipbook/src/pages/index.tsx
@@ -1,16 +1,12 @@
 import { getFlipBook } from '@/ssg/flipbookcontent';
-import { LParser } from '@/ssg/parser';
 import { InferGetStaticPropsType } from 'next';
 import React from 'react';
 import { FlipBook } from '../components/organism/flipbook';
-import styled from '@emotion/styled';
 import { Graduate } from '@/ssg/graduate';
 import { getScienceContent } from '@/ssg/science';
 
 const projectsPage = ({ content, graduate, science }: InferGetStaticPropsType<typeof getStaticProps>) => {
-    return (
-        <FlipBook science={science} graduate={graduate} pages={content}></FlipBook>
-    );
+    return <FlipBook science={science} graduate={graduate} pages={content}></FlipBook>;
 };
 export const getStaticProps = () => {
     return {
