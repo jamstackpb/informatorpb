a11f242d3bfbebcd2b84641b6baf55ac317e812f
ghosttly <ghosttlyg@gmail.com>
2022-04-08 13:07:05 +0200

    wstepny szkielet spisu tresci

--- apps/flipbook/src/models/index.tsx
+++ apps/flipbook/src/models/index.tsx
@@ -1,9 +1,10 @@
 import { getFieldsOfStudy } from '@/ssg/fieldofstudy';
 import { Graduate } from '@/ssg/graduate';
 import { getScienceContent } from '@/ssg/science';
+import { getTableOfContents } from '@/ssg/spistresci';
 
 type KierunekProps = ReturnType<typeof getFieldsOfStudy> extends Array<infer R> ? R : never;
 type ScienceProps = ReturnType<typeof getScienceContent> extends Array<infer R> ? R : never;
 type AbsolwentProps = ReturnType<typeof Graduate> extends Array<infer R> ? R : never;
-
-export type ContentPageProps = KierunekProps | ScienceProps | AbsolwentProps;
+type TableOfContentsProps = ReturnType<typeof getTableOfContents> extends Array<infer R> ? R : never;
+export type ContentPageProps = KierunekProps | ScienceProps | AbsolwentProps | TableOfContentsProps;
